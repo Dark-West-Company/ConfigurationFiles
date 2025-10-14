14dfb6327b7bbbe7f782bfc5cca7b0502b816e1e
Allie <alinaabdurshina@gmail.com>
2025-10-14 03:05:23 +0300

    Update syn_stores_normalstores.lua

--- configs/syn_stores_normalstores.lua
+++ configs/syn_stores_normalstores.lua
@@ -17,8 +17,12 @@ Config.normalstores = {  -- delete everything inside here if you dont want norma
             {name = "meat", label = "Meat", price = "1",type = "item_standard"}, -- change label of items shown
             {name = "consumable_handpie", label = "Meat Handpie", price = "1",type = "item_standard"}, -- change label of items shown
             {name = "consumable_faygo", label = "A little Fizzy Drink", price = "1",type = "item_standard"}, -- change label of items shown
+            {name = "ironhammer", label = "Iron hammer", price = "1",type = "item_standard"}, -- change label of items shown
             {name = "pickaxe", label = "Pickaxe", price = "1",type = "item_standard"}, -- change label of items shown
             {name = "lumberaxe", label = "Cherry Seeds", price = "1",type = "item_standard"}, -- change label of items shown
+             {name = "iron_ore", label = "Iron Ore", price = "1",type = "item_standard"}, -- change label of items shown
+             {name = "wood", label = "Soft Wood", price = "1",type = "item_standard"}, -- change label of items shown
+             {name = "hwood", label = "Hardwood", price = "1",type = "item_standard"}, -- change label of items shown
             {name = "water", label = "water", price = "2",type = "item_standard"}, -- change label of items shown 
             {name = "seed_cherry", label = "Cherry Seeds", price = "1",type = "item_standard"}, -- change label of items shown
             {name = "rake", label = "rake", price = "1",type = "item_standard"}, -- change label of items shown 
